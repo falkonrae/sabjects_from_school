5c1c763c796950ab94e7d486aefad02eb8c0520a
Инна Филимончева <vjacob@et-e1.kzn.21-school.ru>
2020-10-31 17:34:08 +0300

    strlen added

--- intra24/libft/ft_strlen.c
+++ intra24/libft/ft_strlen.c
@@ -1,32 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vjacob <vjacob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/31 15:42:41 by vjacob            #+#    #+#             */
-/*   Updated: 2020/10/31 17:30:51 by vjacob           ###   ########.fr       */
+/*   Created: 2020/10/31 17:10:19 by vjacob            #+#    #+#             */
+/*   Updated: 2020/10/31 17:33:34 by vjacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*memccpy(void *dst, const void *src, int c, size_t n)
+size_t	strlen(const char *s)
 {
-	char	*p;
-	char	*w;
-	char	*pointer;
-	int		i;
+	size_t i;
 
 	i = 0;
-	pointer = (char *)c;
-	p = (char *)dst;
-	w = (char *)src;
-	while (n > 0 && src[i] != c)
+	while (*s)
 	{
-		dst[i] = src[i];
-		n--;
 		i++;
+		s++;
 	}
+	return (i);
 }
