5a13cf75d91b1cbdf7ce63ba3d537cd628574b3b
Инна Филимончева <vjacob@ev-k4.kzn.21-school.ru>
2020-11-06 14:15:09 +0300

    strnstr added

--- intra24/libft/libft.h
+++ intra24/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vjacob <vjacob@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 11:57:16 by vjacob            #+#    #+#             */
-/*   Updated: 2020/11/05 17:35:17 by vjacob           ###   ########.fr       */
+/*   Updated: 2020/11/06 13:58:59 by vjacob           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,4 @@ size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char *ft_strnstr(const char *haystack, const char *needle, size_t len);
