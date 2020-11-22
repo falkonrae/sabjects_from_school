2796bf53e4f920af691b38ce37092487a7c360b8
amarantainn <amarantainn@gmail.com>
2020-11-22 09:08:08 +0300

    lstmap

--- intra24/libft/ft_lstmap.c
+++ intra24/libft/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vjacob <vjacob@student.42.fr>              +#+  +:+       +#+        */
+/*   By: falkonrae <falkonrae@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 17:21:50 by vjacob            #+#    #+#             */
-/*   Updated: 2020/11/10 15:11:29 by vjacob           ###   ########.fr       */
+/*   Updated: 2020/11/22 09:07:32 by falkonrae        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,81 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
 	}
 	return (first);
 }
+
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list *new_list;
+// 	t_list *save;
+
+// 	if (!lst || !f || !del)
+// 		return (0);
+// 	new_list = ft_lstnew(f(lst->content));
+// 	if (!new_list)
+// 		return (0);
+// 	save = new_list;
+// 	lst = lst->next;
+// 	while (lst)
+// 	{
+// 		new_list->next = ft_lstnew(f(lst->content));
+// 		if (!new_list->next)
+// 		{
+// 			ft_lstclear(&save, del);
+// 			return (0);
+// 		}
+// 		new_list = new_list->next;
+// 		lst = lst->next;
+// 	}
+// 	new_list->next = NULL;
+// 	return (save);
+// }
+//---------------------------------------------------------------------------
+
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list *new_list;
+// 	t_list *save;
+
+// 	if (!lst || !f || !del)
+// 		return (0);
+// 	new_list = ft_lstnew(f(lst->content));
+// 	if (!new_list)
+// 		return (0);
+// 	save = new_list;
+// 	lst = lst->next;
+// 	while (lst)
+// 	{
+// 		new_list->next = ft_lstnew(f(lst->content));
+// 		if (!new_list->next)
+// 		{
+// 			ft_lstclear(&save, del);
+// 			return (0);
+// 		}
+// 		new_list = new_list->next;
+// 		lst = lst->next;
+// 	}
+// 	new_list->next = NULL;
+// 	return (save);
+// }
+
+//----------------------------------------------------------------------
+// t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+// {
+// 	t_list		*new;
+// 	t_list		*list;
+
+// 	if (!lst)
+// 		return (NULL);
+// 	list = f(lst);
+// 	new = list;
+// 	while (lst->next)
+// 	{
+// 		lst = lst->next;
+// 		if (!(list->next = f(lst)))
+// 		{
+// 			free(list->next);
+// 			return (NULL);
+// 		}
+// 		list = list->next;
+// 	}
+// 	return (new);
+// }
